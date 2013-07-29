eeee9c44b1efae04206f885dba0e881aaf8bfc04
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-29 14:00:53 -0500

    Done

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  
+  var styling = function(submission){ 
+    var data = {};
+
+    $(submission).find('input').each(function(i, field){
+      data[field.name] = field.value;
+      $(data.selector).css(data.property, data.value);
+    });
+  };
+
+  $('#style_editor').on('submit',function(event){
+    event.preventDefault();
+    styling(this);
+  });
+
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
